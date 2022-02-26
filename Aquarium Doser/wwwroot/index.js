22ea1df0389757290616faa7712cb8e78b227eed
Sam <sam@bandwbuilds.com>
2022-02-26 03:01:21 -0800

    fixed css for index.html, added home page skeleton, added load function to index.html

--- Aquarium Doser/wwwroot/index.js
+++ Aquarium Doser/wwwroot/index.js
@@ -1,2 +1,5 @@
-﻿window.onload = function(){
+﻿window.onload = function () {
+    $("#contentContainer").load("html/home.html", function () {
+        console.log("html/home.html loaded");
+    });
 }
