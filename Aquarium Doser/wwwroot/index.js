9bf75bd62f856eabac0a5b1ffa18c71531100b48
Sam <sam@bandwbuilds.com>
2022-02-27 01:25:31 -0800

    fixed hyperlink refresh bug, fixed css styles for new buttons, bound click events for relevant sidenav buttons

--- Aquarium Doser/wwwroot/index.js
+++ Aquarium Doser/wwwroot/index.js
@@ -1,5 +1,23 @@
-﻿window.onload = function () {
+﻿var site = {};
+site.index = {};
+
+$(document).ready(function () {
     $("#contentContainer").load("html/home.html", function () {
         console.log("html/home.html loaded");
+    });
+    site.index.bindEvents();
+});
+
+site.index.bindEvents = function () {
+    $("#homeListItem").off().click(() => {
+        $("#contentContainer").load("html/home.html", function () {
+            console.log("html/home.html loaded");
+        });
+    });
+
+    $("#doserListItem").off().click(() => {
+        $("#contentContainer").load("html/doserPage.html", function () {
+            console.log("html/doserPage.html loaded");
+        });
     });
 }
