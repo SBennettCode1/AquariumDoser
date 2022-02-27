af2fa77fe596c4085e9041f9eee119a552ba6ea5
Sam <sam@bandwbuilds.com>
2022-02-27 02:56:46 -0800

    Created new guide page and button to access it on the home page, added new content to home page, moved font to body style

--- Aquarium Doser/wwwroot/js/home.js
+++ Aquarium Doser/wwwroot/js/home.js
@@ -1,2 +1,13 @@
-﻿$(document).ready(function () {
+﻿site.home = {};
+
+$(document).ready(function () {
+    site.home.bindEvents();
 });
+
+site.home.bindEvents = function () {
+    $("#userGuideButton").off().click(() => {
+        $("#contentContainer").load("html/guide.html", function () {
+            console.log("html/guide.html loaded");
+        });
+    });
+}
