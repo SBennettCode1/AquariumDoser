94fff0f6a5424af5617fd6b11bb109ddfce1b2f5
Sam <sam@bandwbuilds.com>
2022-03-01 02:14:27 -0800

    Added doserPage.js and added initial functionality, added calculate button with css to doserPage, fixed up doserPage.html

---
+++ Aquarium Doser/wwwroot/js/doserPage.js
@@ -0,0 +1,32 @@
+﻿site.doserPage = {};
+site.doserPage.additiveList = {
+    "additives": [
+        { "name": "Seachem Flourish", "volume": 60, "quantity": 5, "volumeUnits": "US Gallons", "quantityUnits": "mL" },
+        { "name": "Seachem Flourish Excel", "volume": 50, "quantity": 5, "volumeUnits": "US Gallons", "quantityUnits": "mL" }
+    ]
+};
+
+$(document).ready(function () {
+    site.doserPage.bindEvents();
+});
+
+site.doserPage.bindEvents = function () {
+    $("#calculateButton").off().click(() => {
+        let selectedAdditive = site.doserPage.getArrayValueById($("#innerDoserSelect").val());
+        let calculationResult = site.doserPage.doCalculation($("#volumeInput").val(), selectedAdditive.volume, selectedAdditive.quantity);
+        console.log(calculationResult);
+    });
+}
+
+site.doserPage.doCalculation = function (waterVolume, originalVolume, originalQuantity) {
+    let quantityPerUnit = originalQuantity / originalVolume;
+    let finalQuantity = quantityPerUnit * waterVolume;
+    return finalQuantity.toFixed(5);
+}
+
+site.doserPage.getArrayValueById = function (name) {
+    for (let additive of site.doserPage.additiveList.additives) {
+        if (additive.name == name)
+            return additive;
+    }
+}
