84582176b3ea52888fc207916651866b2781e53c
SbennettCode1 <sbennettcode100@gmail.com>
2022-03-16 18:13:27 -0700

    created a new sql file for the default_data table

---
+++ Aquarium Doser/sql/default_data.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [default_data]
+(
+	[id] UNIQUEIDENTIFIER PRIMARY KEY NOT NULL,
+	[name] NVARCHAR(255) NOT NULL,
+	[volume] DECIMAL(10,5) NOT NULL,
+	[volume_units] NVARCHAR(255) NOT NULL,
+	[quantity] DECIMAL(10,5) NOT NULL,
+	[quantity_units] NVARCHAR(255) NOT NULL
+)
