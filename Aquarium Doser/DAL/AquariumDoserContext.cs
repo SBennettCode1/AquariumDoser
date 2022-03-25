ef66e1cd89f336525d9d8bcc3f2210fd7550beda
SbennettCode1 <sbennettcode100@gmail.com>
2022-03-25 01:13:44 -0700

    Created data access layer with context for Aquarium Doser sql db and added matching ContextFactory

---
+++ Aquarium Doser/DAL/AquariumDoserContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aquarium_Doser.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Aquarium_Doser.DAL
+{
+    public class AquariumDoserContext : DbContext
+    {
+        public AquariumDoserContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<DefaultData> DefaultDataObjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<DefaultData>(entity =>
+            {
+                entity.ToTable("default_data", schema: "dbo").HasKey(k => k.ID);
+                entity.Property(dd => dd.ID).HasColumnName("id").IsRequired(true);
+                entity.Property(dd => dd.Name).HasColumnName("name").IsRequired(true);
+                entity.Property(dd => dd.Volume).HasColumnName("volume").IsRequired(true);
+                entity.Property(dd => dd.VolumeUnits).HasColumnName("volume_units").IsRequired(true);
+                entity.Property(dd => dd.Quantity).HasColumnName("quantity").IsRequired(true);
+                entity.Property(dd => dd.QuantityUnits).HasColumnName("quantity_units").IsRequired(true);
+            });
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ Aquarium Doser/DAL/ContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Aquarium_Doser.DAL
+{
+    public class ContextFactory
+    {
+        private DbContextOptions GetDbContextOptions<T>(string connectionString) where T : DbContext
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<T>();
+            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            return optionsBuilder.Options;
+        }
+
+        public AquariumDoserContext GetContext()
+        {
+            return new AquariumDoserContext(GetDbContextOptions<AquariumDoserContext>("Server=tcp:aqudose.database.windows.net,1433;Initial Catalog=AquariumDoser;Persist Security Info=False;User ID=sam;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+        }
+    }
+}
