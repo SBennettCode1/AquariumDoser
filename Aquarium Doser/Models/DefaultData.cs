e755408942b76261f445194d35d17e1054d1d6e4
SbennettCode1 <sbennettcode100@gmail.com>
2022-03-24 23:39:37 -0700

    Added entity framework reference and created model for DefaultData sql table

---
+++ Aquarium Doser/Models/DefaultData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquarium_Doser.Models
+{
+    public class DefaultData
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public double Volume { get; set; }
+        public string VolumeUnits { get; set; }
+        public double Quantity { get; set; }
+        public string QuantityUnits { get; set; }
+    }
+}
