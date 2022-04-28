12103e09dd57d3136eae1bcd70bc299f79464d26
SbennettCode1 <sbennettcode100@gmail.com>
2022-04-27 19:11:25 -0700

    created new js file for addDosage page with ajax post method and made minor changes to html

---
+++ Aquarium Doser/wwwroot/js/addDosage.js
@@ -0,0 +1,35 @@
+﻿site.addDosage = {};
+
+$(document).ready(function () {
+    site.addDosage.bindEvents();
+});
+
+site.addDosage.bindEvents = function () {
+    $("#addDosageButton").off().click(() => {
+        var url = `api/NewUserData`;
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: url,
+
+            data: JSON.stringify({
+                "name": $("#addDosageNameInput").val(),
+                "volume": Number($("#addDosageVolumeInput").val()),
+                "volumeUnits": $("#addDosageVolumeUnitInput option:selected").text(),
+                "quantity": Number($("#addDosageQuantityInput").val()),
+                "quantityUnits": $("#addDosageQuantityUnitInput option:selected").text(),
+                "email": site.userInfo.Bv
+            }),
+
+            success: function (json) {
+                console.log(json);
+            },
+
+            error: function () {
+                console.error("post user data ajax call failed");
+            }
+        });
+    });
+}
